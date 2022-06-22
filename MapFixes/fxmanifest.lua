8d116e4e802cf2c22127c49d6a8e96babcbd5195
TayMcKenzieSA <taylorocarroll@gmail.com>
2022-06-22 20:42:40 +1200

    Updated MapFixes

--- MapFixes/fxmanifest.lua
+++ MapFixes/fxmanifest.lua
@@ -1,4 +1,26 @@
-fx_version 'adamant'
+--- Map Fixes By LUKY00010  ---
+--- Compiled By TayMcKenzieNZ ---
+
+
+fx_version 'cerulean'
 game 'gta5'
 
+
 this_is_a_map 'yes'
+
+
+--- FIXES ---
+
+
+--- Fixed walls and stunt jump around the job center ---
+--- Fixed jumps around the FBI building. Remove  dt1_rd1_0.ybn file from any Pillbox Hospital MLOs as it will conflict
+--- Fixed Tattoo Parlor 'The Pits' Construction
+--- Fixed broken wall near LSIA 
+--- Fixed stunt jump and construction near FBI building
+--- Fixed broken bridge at the Golf Club
+--- Fixed Korean Pavilion
+
+
+--- If you face any conflicts, check the files are not duplicated with any ymaps or nearby MLOs ---
+
+--- Remove construction NPCs etc using my server_scenarios resource
