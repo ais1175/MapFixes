f29ecc203668ac6dccb6fd24d02867916eaacc28
TayMcKenzieSA <taylorocarroll@gmail.com>
2022-08-31 23:33:18 +1200

    Attempted To Fix Invisible Roads

--- MapFixes/fxmanifest.lua
+++ MapFixes/fxmanifest.lua
@@ -12,13 +12,14 @@ this_is_a_map 'yes'
 --- FIXES ---
 
 
---- Fixed walls and stunt jump around the job center ---
+--- Fixed walls and stunt jump around the job center
 --- Fixed jumps around the FBI building. Remove  dt1_rd1_0.ybn file from any Pillbox Hospital MLOs as it will conflict
 --- Fixed Tattoo Parlor 'The Pits' Construction
 --- Fixed broken wall near LSIA 
 --- Fixed stunt jump and construction near FBI building
 --- Fixed broken bridge at the Golf Club
 --- Fixed Korean Pavilion
+--- Fixed tunnel hole in the wall on Del Perro Freeway
 
 
 --- If you face any conflicts, check the files are not duplicated with any ymaps or nearby MLOs ---
